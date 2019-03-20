8f61cdd1af5a8e20fc7ab3748d998ec4002146a5
Brian Burkard <brianburkard@gmail.com>
2019-03-19 20:18:27 -0400

    first commit

---
+++ game.js
@@ -0,0 +1,16 @@
+gameState = {};
+
+function preload(){
+
+}
+
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: 0x84ba75,
+    scene: {
+        preload,
+    }
+}
