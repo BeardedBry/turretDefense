e3ee1e75938c7854da65f86f2574d5fae07a1e0f
Brian Burkard <brianburkard@gmail.com>
2019-03-20 19:47:22 -0400

    scene

--- game.js
+++ game.js
@@ -1,16 +1,20 @@
-gameState = {};
+//gameState = {};
 
-function preload(){
 
+function create(){
+    let controlBox = this.add.rectangle(0,475, 440, 75,50, 0x37c3be,100);
+    controlBox.setOrigin(0,0);
 }
 
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 500,
     height: 600,
     backgroundColor: 0x84ba75,
     scene: {
-        preload,
-    }
+        create,
+    },
 }
+
+const game = new Phaser.Game(config)
